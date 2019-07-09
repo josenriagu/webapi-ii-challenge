8debb5eac12e49057be0fbc07cbafa66713267ca
Josemaria <josemarianriagu25@gmail.com>
2019-07-09 20:31:31 +0800

    implement GET by ID api

--- data/db-routes.js
+++ data/db-routes.js
@@ -14,9 +14,27 @@ route.get('/', async (req, res) => {
       // log error to database
       console.log(error);
       res.status(500).json({
-         message: 'Error retrieving the posts',
+         error: "The posts information could not be retrieved."
       });
    }
 });
+
+//get posts by id
+route.get('/:id', async (req, res) => {
+   const { id } = req.params;
+   try {
+      const posts = await Posts.findById(id);
+      // posts returns an empty array if an id does not exist
+      if (id && posts.length !== 0) {
+         res.status(200).json(posts);
+      } else {
+         res.status(404).json({ message: "We found something on that id, but it disappeared into the blackHole!" })
+      }
+   } catch (error) {
+      res.status(500).json({
+         error: "The posts information could not be retrieved."
+      })
+   }
+})
 
 module.exports = route;
